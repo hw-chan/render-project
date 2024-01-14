0d38d28bb5bb715ff0f8492f4ae0d4952112dfcd
Hw Chan <hwchan12@outlook.com>
2024-01-14 20:14:13 +0800

    add mongoDB

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const cors = require("cors");
 var morgan = require("morgan");
 
+const Person = require('./models/person')
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cors());
 app.use(express.static('dist'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(result => {
+    response.json(result)
+  })
 });
 
 app.get("/info", (request, response) => {
@@ -47,20 +39,16 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })  
+    .catch(error => next(error))
+}); 
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -71,30 +59,27 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  let entry = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
-
-  persons.push(entry);
-  response.json(entry);
+  })
+  
+  person.save().then(result => {
+    response.json(result)
+  })
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error));
 });
 
-const PORT = process.env.PORT || 3001
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
